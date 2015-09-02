9ee3862b2ae9b32daa770aa33a5bf61ba574be6f
Owo Sugiana <sugiana@gmail.com>
2015-09-02 15:43:36 +0700

    Penambahan cleaner agar siap insert ke database

---
+++ bhinneka/cleaner/notebook.py
@@ -0,0 +1,156 @@
+import json
+from tools import (
+    REGEX_UNITS,
+    copy_text,
+    regex_compile,
+    c_regex_search,
+    clean,
+    price,
+    key_not_found,
+    unknown_values,
+    )
+
+
+MEMORY_REGEXS = [
+    '(\d*) %s' % REGEX_UNITS, # 2 GB DDR-3
+    '(\d*)%s' % REGEX_UNITS, # 8GB of 1600MHz LPDDR3 onboard memory
+    ]
+
+STORAGE_REGEXS = [
+    '(\d*) %s' % REGEX_UNITS, # 500 GB HDD
+    '(\d*)%s' % REGEX_UNITS, # 256GB PCIe-based onboard flash storage
+    ]
+
+RESOLUTION_REGEXS = [
+    '(\d*)-by-(\d*)', # 2304-by-1440 resolution at 226 pixels per inch with support for millions of colors
+    '(\d*) x (\d*)', # 1366 x 768
+    '(\d*)x(\d*)', # 1920x1200
+    ]
+
+MONITOR_REGEXS = [
+    '(\d*\.\d) inch', # 12.5 inch
+    '(\d*\.\d)"', # 12.5"
+    '(\d*\.\d)" wxga', # 10.1" wxga
+    '(\d*\.\d) wxga', # 10.1 wxga
+    '(\d*)" wxga', # 12" wxga
+    '(\d*) inch', # 12 inch
+    '(\d*)"', # 12"
+    '(\d*\.\d)', # 20.0 
+    '(\d*)', # 20.0 
+    ]
+
+
+class NotebookCleaner(object):
+    memory_regexs = MEMORY_REGEXS
+    storage_regexs = STORAGE_REGEXS
+    resolution_regexs = RESOLUTION_REGEXS
+    monitor_regexs = MONITOR_REGEXS
+
+    def __init__(self):
+        self.memory_regexs_compiled = regex_compile(self.memory_regexs)
+        self.storage_regexs_compiled = regex_compile(self.storage_regexs)
+        self.resolution_regexs_compiled = regex_compile(self.resolution_regexs)
+        self.monitor_regexs_compiled = regex_compile(self.monitor_regexs)
+
+    def parse(self, data):
+        def parse_(key, func):
+            if key in data:
+                values = data[key]
+                if not values:
+                    return
+                val = func(values)
+                if not val:
+                    print(data)
+                    unknown_values(key, values)
+                r[key] = val
+            else:
+                key_not_found(key, data['url'])
+
+        r = copy_text(data)
+        parse_('price', self.parse_price)
+        parse_('memory', self.parse_memory)
+        parse_('storage', self.parse_storage)
+        parse_('resolution', self.parse_resolution)
+        parse_('monitor', self.parse_monitor)
+        return r
+
+    def parse_price(self, value):
+        s = clean(value)
+        vals = price(value)
+        if vals:
+            amount, currency = vals
+            return [value, amount, currency]
+
+    def parse_memory(self, values):
+        for value in values:
+            s = clean(value).lower()
+            for rc in self.memory_regexs_compiled:
+                match = c_regex_search(rc, s)
+                if match:
+                    amount = match.group(1)
+                    unit = match.group(2)
+                    return [value, int(amount), unit]
+
+    def parse_storage(self, values):
+        for value in values:
+            s = clean(value).lower()
+            for rc in self.storage_regexs_compiled:
+                match = c_regex_search(rc, s)
+                if match:
+                    amount = match.group(1)
+                    unit = match.group(2)
+                    return [value, int(amount), unit]
+
+    def parse_resolution(self, values):
+        for value in values:
+            s = clean(value).lower()
+            for rc in self.resolution_regexs_compiled:
+                match = c_regex_search(rc, s)
+                if match:
+                    width = match.group(1)
+                    height = match.group(2)
+                    try:
+                        return [value, int(width), int(height)]
+                    except ValueError, err:
+                        print(err)
+                        return
+
+    def parse_monitor(self, values):
+        for value in values:
+            s = clean(value).lower()
+            for rc in self.monitor_regexs_compiled:
+                match = c_regex_search(rc, s)
+                if match:
+                    amount = match.group(1)
+                    if amount:
+                        return [value, float(amount)]
+
+    def parse_json(self, f):
+        results = []
+        rows = json.load(f)
+        for row in rows:
+            r = self.parse(row)
+            s = json.dumps(r)
+            results.append(s)
+        return '[%s]' % ',\n'.join(results)
+
+
+if __name__ == '__main__':
+    import sys
+    from optparse import OptionParser
+    pars = OptionParser()
+    pars.add_option('-i', '--input', help='JSON file')
+    pars.add_option('-o', '--output', help='JSON file')
+    option, remain = pars.parse_args(sys.argv[1:])
+    if not option.output:
+        print('--output harus diisi.')
+        sys.exit()
+    input_file = option.input
+    output_file = option.output
+    cleaner = NotebookCleaner()
+    f = open(input_file)
+    s = cleaner.parse_json(f)
+    f.close()
+    f = open(output_file, 'w')
+    f.write(s)
+    f.close()
---
+++ bhinneka/cleaner/tools.py
@@ -0,0 +1,104 @@
+import re
+import string
+from types import ListType
+from bs4 import BeautifulSoup
+
+
+##########
+# String #
+##########
+def just_ascii(s):
+    r = ''
+    for ch in s:
+        if ord(ch) > 127:
+            ch = ' '
+        r += ch
+    return r
+
+def clean_char(ch):
+    if ch == u'\u201d':
+        return '"'
+    return ch in string.printable and ch or ' '
+
+def one_space(s):
+    while s.find('  ') > -1:
+        s = s.replace('  ', ' ')
+    return s
+    
+def clean(s):
+    s = ''.join([clean_char(ch) for ch in s])
+    s = one_space(s)
+    if s.find('<span') < 0 and s.find('<br') < 0:
+        return s
+    soup = BeautifulSoup(s, 'lxml')
+    for link in soup.find_all('span'):
+        link.unwrap()
+    s = str(soup).replace('<br/>', '\n').\
+            lstrip('<html><body><p>').\
+            rstrip('</p></body></html>')
+    return s
+    
+def v(values):
+    s = ' '.join(values)
+    return s.strip()
+
+######################
+# Regular Expression #
+######################
+def regex_compile(regexs):
+    r = []
+    for regex in regexs:
+        if type(regex) == ListType:
+            regex = regex[0]
+        c = re.compile(regex)
+        r.append(c)
+    return r
+    
+def c_regex_search(c_regex, text):
+    return c_regex.search(text.lower())
+    
+def regex_search(regex, text):
+    return re.compile(regex.lower()).search(text.lower())        
+    
+
+#########
+# Price #
+#########
+def price(s):
+    val = s.lower().replace('rp', 'idr').replace(',', '')
+    if val:
+        currency, amount = val.split()
+        return [int(amount), currency]
+
+##########
+# Memory #
+##########
+UNITS = ['mb', 'gb', 'tb']
+REGEX_UNITS = '(%s)' % '|'.join(UNITS)
+
+
+TEXT_KEYS = ['brand', 'picture', 'description', 'title', 'url', 'battery',
+             'graphic', 'processor']
+
+def copy_text(data):
+    r = dict()
+    for key in TEXT_KEYS:
+        if key in data:
+            r[key] = data[key]
+    if 'description' in r:
+        r['description'] = clean(r['description'])
+    return r
+
+#################
+# Error handler #
+#################
+def unknown_values(key, values):
+    print('GAGAL menerjemahkan nilai dalam {k}:'.format(k=key))
+    for value in values:
+        print([value])
+    raise Exception('Tidak ditemukan pola yang cocok.')
+
+def key_not_found(key, url):
+    print('TIDAK ADA {k} di {u}'.format(k=key, u=url))
+
+
